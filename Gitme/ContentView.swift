dacf665141ffd66e2f1614703e4563433fab4ae8
vzooot <vzotikas@hotmail.com>
2023-02-13 20:45:58 +0100

    Branch02 Commit01

--- Gitme/ContentView.swift
+++ Gitme/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("Main 02!")
+            Text("Branch01 Commit01!")
         }
         .padding()
     }
